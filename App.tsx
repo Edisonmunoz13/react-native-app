d05edea2d6caefab03e3ba07187544c2c5020efb
Edisonmunoz13 <horacioem595>
2022-12-22 23:41:30 -0300

    feature: transactions

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import data from "./assets/data.json";
 import { StyleSheet, Text, View } from "react-native";
 
 const user = data.user;
+const transactions = data.transactions;
 console.log(user);
 
 export default function App() {
@@ -13,17 +14,24 @@ export default function App() {
         <Text style={styles.text}> {user.email}</Text>
 
         <View style={styles.section}>
-          <Text style={styles.text}>Accounts</Text>
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
-          >
-            <View style={{ width: 50, height: 50, backgroundColor: "red" }}>
-              <Text>{}</Text>
+          <Text style={styles.text}>Account</Text>
+          {user.accounts.map((x) => (
+            <View style={styles.account}>
+              <Text style={styles.column1}> {x.type} </Text>
+              <Text style={styles.column2}> $ {x.balance} </Text>
             </View>
-            <View
-              style={{ width: 50, height: 50, backgroundColor: "green" }}
-            ></View>
-          </View>
+          ))}
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.text}>Transactions</Text>
+          {transactions.map((x) => (
+            <View style={styles.account}>
+              <Text style={styles.column}> {x.type} </Text>
+              <Text style={styles.column}> {x.account} </Text>
+              <Text style={styles.column3}> $ {x.amount} </Text>
+            </View>
+          ))}
         </View>
 
         <StatusBar style="auto" />
@@ -39,18 +47,20 @@ export default function App() {
 const styles = StyleSheet.create({
   title: {
     fontStyle: "normal",
+    fontSize: 22,
     fontWeight: "bold",
     color: "rgb(56,56,56)",
-    padding: 50,
+    padding: 15,
   },
   background: {
     width: "100%",
     display: "flex",
   },
   text: {
+    padding: 5,
     fontStyle: "normal",
     fontWeight: "bold",
-    color: "rgb(56,56,56)",
+    color: "rgb(99,99,99)",
     marginBottom: 30,
   },
   container: {
@@ -65,9 +75,46 @@ const styles = StyleSheet.create({
   },
   section: {
     backgroundColor: "white",
-    height: 300,
+    marginBottom: 50,
     width: "80%",
     alignItems: "center",
     borderRadius: 15,
+  },
+  account: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 15,
+  },
+  column1: {
+    fontStyle: "normal",
+    fontWeight: "bold",
+    color: "rgb(99,99,99)",
+    padding: 5,
+    width: "50%",
+  },
+  column2: {
+    fontStyle: "normal",
+    fontWeight: "bold",
+    color: "rgb(99,99,99)",
+    padding: 5,
+    width: "45%",
+    textAlign: "right",
+  },
+  column3: {
+    fontStyle: "normal",
+    fontWeight: "bold",
+    color: "rgb(99,99,99)",
+    padding: 5,
+    width: "33%",
+    textAlign: "right",
+  },
+  column: {
+    fontStyle: "normal",
+    fontWeight: "bold",
+    color: "rgb(99,99,99)",
+    padding: 5,
+    width: "33%",
   },
 });
